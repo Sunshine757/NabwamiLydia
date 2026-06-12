a556be257db333cac07ed5628e43a6dfd1d5e2f8
Your Name <you@example.com>
2026-06-12 15:18:17 +0300

    Initial commit: Python exercise files

---
+++ lydia.py
@@ -0,0 +1,13 @@
+
+
+# output operation
+#print("I love Python Language")
+
+#formated string
+name = "Lydia"
+age = 22
+height = 2.6
+print(f"My name is {name}, i am {age}years old and my height is {height} meters ")
+
+
+
